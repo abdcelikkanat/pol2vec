Position of ordinal vars, precision matrix for each step, diagonal terms for precision mat, sigma value fixed.

examples/example1_n=100_m=50.py
@@ -13,12 +13,12 @@ base_folder = os.path.realpath(os.path.join(script_folder, ".."))
 dataset_folder = os.path.realpath(os.path.join(base_folder, "datasets"))
 
 # Set the dataset name
-dataset_name = "example1_n=10_m=5"  #"example1"
+dataset_name = "example1_n=100_m=50"  #"example1"
 
 # Set the model parameters
 dim = 2
 order = 2
-epochs_num = 50
+epochs_num = 100
 learning_rate = 0.1
 samples_num = 10
 seed = 123
@@ -56,7 +56,7 @@ plt.savefig(loss_file_path)
 animation_file_path = os.path.join(base_folder, "figures", "{}_animation.gif".format(filename_name))
 
 # Sample some time points for the visualization
-time_points_num = 100
+time_points_num = data['events'].shape[1]
 time_points = np.linspace(0, 1, time_points_num)
 
 # Get the correct latent positions
@@ -74,8 +74,8 @@ for t_idx, t in enumerate(time_points):
 anim = Animation(timePoints=time_points,
                          r=1, c=2, figSize=(16, 6), bgColor='white',
                          # color='k', #
-                        color=['r' if i in data['groups'][0] else 'k' for i in range(data['events'].shape[0])],
-                         marker='.', markerSize=15, delay=250, margin=[0.1, 10], label=False)
+                        color=[['r' if data['events'][i, bill_idx] else 'k' for i in range(data['events'].shape[0])] for bill_idx in range(data['events'].shape[1])],
+                         marker='.', markerSize=10, delay=1000, margin=[0.1, 10], label=False)
 anim.addData(x=z_true, index=0, title="True")
 anim.addData(x=z_est, index=1, title="Estimation")
 anim.plot(filePath=animation_file_path)

examples/pol2vecbinary_example1.py
@@ -1,10 +1,26 @@
-import os
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import matplotlib.pyplot as plt
-from models.pol2vec import *
+from models.pol2vecbinary import *
 from datasets.loader import *
 from utilities.animation import *
 from utilities.complete_motion import CompleteMotion
 torch.set_num_threads(16)
+
+
+def batching(event_mat, event_times, batch_size):
+    event_mat_list = []
+    event_times_list = []
+    col_indices_list = []
+
+    for i in range(0, event_mat.shape[1], batch_size):
+        event_mat_list.append(event_mat[:, i:i + batch_size])
+        col_indices_list.append(list(range(i, i+batch_size)))
+        event_times_list.append(event_times[i:i + batch_size])
+
+    return event_mat_list, col_indices_list, event_times_list
 
 
 # Define the global variables
@@ -13,7 +29,8 @@ base_folder = os.path.realpath(os.path.join(script_folder, ".."))
 dataset_folder = os.path.realpath(os.path.join(base_folder, "datasets"))
 
 # Set the dataset name
-dataset_name = "example1_n=100_m=50"  #"example1"
+dataset_name = "arxiv_19-20" #"arxiv_19-20"  #"example1
+suffix = "example1"
 
 # Set the model parameters
 dim = 2
@@ -21,61 +38,94 @@ order = 2
 epochs_num = 100
 learning_rate = 0.1
 samples_num = 10
+batch_size = 5
 seed = 123
-
-# Load the dataset
-data = load(name=dataset_name, dataset_type="syn")
-# Normalize time list
-data['times'] = [value / float(max(data['times'])) for value in data['times']]
-
-# Define the model
-p2v = Pol2Vec(train_data=data, dim=dim, order=order, epochs_num=epochs_num,
-              learning_rate=learning_rate, samples_num=samples_num, seed=seed)
-# Train the model
-train_loss_list, test_loss_list = p2v.learn(type="seq")
-
+learn = True
 
 # Define the file name
-filename_name = "{}_epochs={}_lr={}_seed={}".format(dataset_name, epochs_num, learning_rate, seed)
+filename = "{}_{}_epochs={}_lr={}_batch_size={}_seed={}".format(dataset_name, suffix, epochs_num, learning_rate, batch_size, seed)
+
+# Set the figures folder
+figures_folder = os.path.join(base_folder, "figures", filename)
+
+# Create the folder
+if not os.path.exists(figures_folder):
+    os.makedirs(figures_folder)
 
 # Define the path for the model parameters
-model_file_path = os.path.join(script_folder, filename_name+".model")
-torch.save(p2v.state_dict(), model_file_path)
-# p2v.load_state_dict(torch.load(model_file_path))
+model_file_path = os.path.join(figures_folder, filename+".model")
 
-# Save the training loss
-loss_file_path = os.path.join(base_folder, "figures", "{}_train_loss.png".format(filename_name))
-plt.figure()
-plt.title("Training Loss")
-plt.xlabel("Epochs")
-plt.ylabel("Negative Log Likelihood")
-plt.plot(train_loss_list, 'k.-')
-plt.savefig(loss_file_path)
+# Define the file path for the training loss
+loss_file_path = os.path.join(figures_folder, "{}_train_loss.png".format(filename))
 
-# Set the file path for the animation
-animation_file_path = os.path.join(base_folder, "figures", "{}_animation.gif".format(filename_name))
+# Load the dataset
+data = load(name=dataset_name, dataset_type="real")
 
-# Sample some time points for the visualization
-time_points_num = data['events'].shape[1]
-time_points = np.linspace(0, 1, time_points_num)
+# Normalize time list
+max_el = float(max(data['times']))
+data['times'] = [value / max_el for value in data['times']]
 
-# Get the correct latent positions
-cm = CompleteMotion(data['z_p'], data['times'])
-z_true = np.zeros(shape=(time_points_num, data['events'].shape[0], dim), dtype=np.float)
-for t_idx, t in enumerate(time_points):
-    z_true[t_idx, :, :] = cm.get_current_positions(t=t)
+# Concert the data to dense format
+data['events'] = data['events'] #.todense()
 
-# Get the estimated positions
-z_est = np.zeros(shape=(time_points_num, data['events'].shape[0], dim), dtype=np.float)
-for t_idx, t in enumerate(time_points):
-    z_est[t_idx, :, :] = p2v.get_all_politician_positions(t=t).detach().numpy()
+print(data['events'].shape)
 
-# Perform the animation
-anim = Animation(timePoints=time_points,
-                         r=1, c=2, figSize=(16, 6), bgColor='white',
-                         # color='k', #
-                        color=[['r' if data['events'][i, bill_idx] else 'k' for i in range(data['events'].shape[0])] for bill_idx in range(data['events'].shape[1])],
-                         marker='.', markerSize=10, delay=1000, margin=[0.1, 10], label=False)
-anim.addData(x=z_true, index=0, title="True")
-anim.addData(x=z_est, index=1, title="Estimation")
-anim.plot(filePath=animation_file_path)
+data['events'], data['col_indices'], data['times'] = batching(event_mat=data['events'], event_times=data['times'], batch_size=batch_size)
+
+
+
+# Define the model
+p2v = Pol2VecBinary(train_data=data, dim=dim, order=order, epochs_num=epochs_num,
+                    learning_rate=learning_rate, samples_num=samples_num, seed=seed)
+
+if learn:
+    print("Learn")
+    # Train the model
+    train_loss_list, test_loss_list = p2v.learn(type="seq")
+
+    # Save the model
+    torch.save(p2v.state_dict(), model_file_path)
+
+    # Save the training loss
+    plt.figure()
+    plt.title("Training Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Negative Log Likelihood")
+    plt.plot(train_loss_list, 'k.-')
+    plt.savefig(loss_file_path)
+
+    p2v.load_state_dict(torch.load(model_file_path))
+
+    print("Completed.")
+else:
+    pass
+
+
+
+# # Set the file path for the animation
+# animation_file_path = os.path.join(base_folder, "figures", "{}_animation.gif".format(filename_name))
+#
+# # Sample some time points for the visualization
+# time_points_num = data['events'].shape[1]
+# time_points = np.linspace(0, 1, time_points_num)
+#
+# # Get the correct latent positions
+# cm = CompleteMotion(data['z_p'], data['times'])
+# z_true = np.zeros(shape=(time_points_num, data['events'].shape[0], dim), dtype=np.float)
+# for t_idx, t in enumerate(time_points):
+#     z_true[t_idx, :, :] = cm.get_current_positions(t=t)
+#
+# # Get the estimated positions
+# z_est = np.zeros(shape=(time_points_num, data['events'].shape[0], dim), dtype=np.float)
+# for t_idx, t in enumerate(time_points):
+#     z_est[t_idx, :, :] = p2v.get_all_politician_positions(t=t).detach().numpy()
+#
+# # Perform the animation
+# anim = Animation(timePoints=time_points,
+#                          r=1, c=2, figSize=(16, 6), bgColor='white',
+#                          # color='k', #
+#                         color=[['r' if data['events'][i, bill_idx] else 'k' for i in range(data['events'].shape[0])] for bill_idx in range(data['events'].shape[1])],
+#                          marker='.', markerSize=10, delay=1000, margin=[0.1, 10], label=False)
+# anim.addData(x=z_true, index=0, title="True")
+# anim.addData(x=z_est, index=1, title="Estimation")
+# anim.plot(filePath=animation_file_path)

examples/pol2vecbinary_multi.py
@@ -3,21 +3,26 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import matplotlib.pyplot as plt
-from models.pol2vecbinary import *
+from models.pol2vecmulti import *
 from datasets.loader import *
 from utilities.animation import *
 from utilities.complete_motion import CompleteMotion
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import MultiLabelBinarizer
 torch.set_num_threads(16)
 
 
-def batching(event_mat, event_times, batch_size):
+def batching(event_mat, event_times, batch_size, max_time):
+    assert batch_size <= max_time, "Batch size must be smaller than max time."
     event_mat_list = []
     event_times_list = []
     col_indices_list = []
 
-    for i in range(0, event_mat.shape[1], batch_size):
+    # for i in range(0, event_mat.shape[1], batch_size):
+    for i in range(0, max_time, batch_size):
+
         event_mat_list.append(event_mat[:, i:i + batch_size])
-        col_indices_list.append(list(range(i, i+batch_size)))
+        col_indices_list.append(list(range(i, i + batch_size)))
         event_times_list.append(event_times[i:i + batch_size])
 
     return event_mat_list, col_indices_list, event_times_list
@@ -29,21 +34,25 @@ base_folder = os.path.realpath(os.path.join(script_folder, ".."))
 dataset_folder = os.path.realpath(os.path.join(base_folder, "datasets"))
 
 # Set the dataset name
-dataset_name = "arxiv_19-20" #"arxiv_19-20"  #"example1
-suffix = "example1"
+dataset_name = "congress" #"arxiv_19-20"  #"example1
+suffix = "deneme" #"test"
 
 # Set the model parameters
 dim = 2
 order = 2
+metric = "mah"
+metric_param = "diag"
 epochs_num = 100
 learning_rate = 0.1
-samples_num = 10
 batch_size = 5
+max_column = 10
+sigma_grad = False
 seed = 123
+verbose = True
 learn = True
 
 # Define the file name
-filename = "{}_{}_epochs={}_lr={}_batch_size={}_seed={}".format(dataset_name, suffix, epochs_num, learning_rate, batch_size, seed)
+filename = "{}_{}_order={}_{}_{}_epochs={}_lr={}_batch_size={}_max_column={}_seed={}".format(dataset_name, suffix, order, metric, metric_param, epochs_num, learning_rate, batch_size, max_column, seed)
 
 # Set the figures folder
 figures_folder = os.path.join(base_folder, "figures", filename)
@@ -60,24 +69,19 @@ loss_file_path = os.path.join(figures_folder, "{}_train_loss.png".format(filenam
 
 # Load the dataset
 data = load(name=dataset_name, dataset_type="real")
-
 # Normalize time list
 max_el = float(max(data['times']))
 data['times'] = [value / max_el for value in data['times']]
-
 # Concert the data to dense format
-data['events'] = data['events'] #.todense()
+data['events'] = data['events']
 
-print(data['events'].shape)
-
-data['events'], data['col_indices'], data['times'] = batching(event_mat=data['events'], event_times=data['times'], batch_size=batch_size)
-
-
-
+# Batching
+train_data = dict()
+train_data['events'], train_data['col_indices'], train_data['times'] = batching(event_mat=data['events'], event_times=data['times'],
+                                                                                batch_size=batch_size, max_time=max_column)
 # Define the model
-p2v = Pol2VecBinary(train_data=data, dim=dim, order=order, epochs_num=epochs_num,
-                    learning_rate=learning_rate, samples_num=samples_num, seed=seed)
-
+p2v = Pol2VecMulti(train_data=train_data, dim=dim, order=order, metric=metric, metric_param=metric_param,
+                   epochs_num=epochs_num, learning_rate=learning_rate, sigma_grad=sigma_grad, seed=seed, verbose=verbose)
 if learn:
     print("Learn")
     # Train the model
@@ -94,38 +98,144 @@ if learn:
     plt.plot(train_loss_list, 'k.-')
     plt.savefig(loss_file_path)
 
-    p2v.load_state_dict(torch.load(model_file_path))
-
     print("Completed.")
 else:
-    pass
+    p2v.load_state_dict(torch.load(model_file_path))
+
+    # Set the file path for the animation
+    animation_file_path = os.path.join(figures_folder, "{}_animation.gif".format(filename))
+
+    # Sample some time points for the visualization
+    time_points_num = max_column
+    time_points = np.linspace(0, 1, time_points_num)
+
+    # Get the column times list in the chosen interval
+    col_times = data['times'][:max_column]
+    # Get the non-zero indices in the chosen interval
+    mat_indices = torch.tensor(data['events'][:, :max_column].todense(), dtype=torch.int8).nonzero()
+    # Get the number of rows in the chosen interval
+    number_of_rows = max(mat_indices.T[0])+1
+    # Construct the estimated embedding tensor which is of size T x N x D
+    z_est = np.zeros(shape=(time_points_num, number_of_rows, dim), dtype=np.float)
+    # Get the estimated positions
+    z_est[mat_indices.T[1], mat_indices.T[0], :] = p2v.get_all_row_positions(mat_indices=mat_indices, col_times=col_times).detach().numpy()
+
+    # Perform the animation
+    color_mat = []
+    for t_idx in range(time_points_num):
+        current_colors = []
+        for node in range(data['events'].shape[0]):
+            if data['events'][node, t_idx] == 1:  # no
+                current_colors.append('maroon')
+            elif data['events'][node, t_idx] == 2:  # yes
+                current_colors.append('navy')
+            elif data['events'][node, t_idx] == 0:
+                current_colors.append('white')
+            else:
+                current_colors.append('yellowgreen')
+        color_mat.append(current_colors)
+
+    anim = Animation(timePoints=time_points, r=1, c=1, figSize=(8, 6), bgColor='white', color=color_mat,
+                     marker='.', markerSize=10, delay=500, margin=[0.1, 0.1], label=False)
+    anim.addData(x=z_est, index=0, canvas=None,  title="Estimation")
+    anim.plot(filePath=animation_file_path)
 
 
+    # Assignment
+    y_assignment = p2v.get_label_assignments(mat_indices=mat_indices, col_times=data['times'][:max_column])
+    # print("Second")
+    # # Set the file path for the animation
+    # animation_file_path2 = os.path.join(figures_folder, "{}_animation2.gif".format(filename))
+    # color_mat = []
+    # for t_idx in range(time_points_num):
+    #     current_colors = []
+    #     for node in range(data['events'].shape[0]):
+    #         current_colors.append('white')
+    #     color_mat.append(current_colors)
+    #
+    # for idx, mat_idx in enumerate(mat_indices):
+    #     if y_assignment[idx] == 0:
+    #         color_mat[mat_idx[1]][mat_idx[0]] = 'r'  # no
+    #     elif y_assignment[idx] == 1:  # yes
+    #         color_mat[mat_idx[1]][mat_idx[0]] = 'b'
+    #     elif y_assignment[idx] == 2:  # yes
+    #         color_mat[mat_idx[1]][mat_idx[0]] = 'green'
+    #
+    # anim = Animation(timePoints=time_points, r=1, c=1, figSize=(8, 6), bgColor='white', color=color_mat,
+    #                  marker='.', markerSize=3, delay=1000, margin=[0.1, 10], label=False)
+    # anim.addData(x=z_est, index=0, title="Estimation")
+    # anim.plot(filePath=animation_file_path2)
 
-# # Set the file path for the animation
-# animation_file_path = os.path.join(base_folder, "figures", "{}_animation.gif".format(filename_name))
-#
-# # Sample some time points for the visualization
-# time_points_num = data['events'].shape[1]
-# time_points = np.linspace(0, 1, time_points_num)
-#
-# # Get the correct latent positions
-# cm = CompleteMotion(data['z_p'], data['times'])
-# z_true = np.zeros(shape=(time_points_num, data['events'].shape[0], dim), dtype=np.float)
-# for t_idx, t in enumerate(time_points):
-#     z_true[t_idx, :, :] = cm.get_current_positions(t=t)
-#
-# # Get the estimated positions
-# z_est = np.zeros(shape=(time_points_num, data['events'].shape[0], dim), dtype=np.float)
-# for t_idx, t in enumerate(time_points):
-#     z_est[t_idx, :, :] = p2v.get_all_politician_positions(t=t).detach().numpy()
-#
-# # Perform the animation
-# anim = Animation(timePoints=time_points,
-#                          r=1, c=2, figSize=(16, 6), bgColor='white',
-#                          # color='k', #
-#                         color=[['r' if data['events'][i, bill_idx] else 'k' for i in range(data['events'].shape[0])] for bill_idx in range(data['events'].shape[1])],
-#                          marker='.', markerSize=10, delay=1000, margin=[0.1, 10], label=False)
-# anim.addData(x=z_true, index=0, title="True")
-# anim.addData(x=z_est, index=1, title="Estimation")
-# anim.plot(filePath=animation_file_path)
+    # --
+
+
+    # values = p2v.compute_all_f(mat_indices=mat_indices, col_times=col_times)
+    # true labels
+    y_true = []
+    for row_idx, col_idx in mat_indices:
+        y_true.append( [ data['events'][row_idx, col_idx] - 1 ] )
+    # estimated labels
+    y_pred = [[l] for l in y_assignment]
+
+    K = len(set([y for yy in y_true for y in yy]))
+
+    # # Find the predictions, each node can have multiple labels
+    # test_prob = np.asarray(ovr.predict_proba(test_features))
+    # y_pred = []
+    # for i in range(test_labels.shape[0]):
+    #     k = test_labels[i].getnnz()  # The number of labels to be predicted
+    #     pred = test_prob[i, :].argsort()[-k:]
+    #     y_pred.append(pred)
+    #
+    # Find the true labels
+    # y_true = [[] for _ in range(test_labels.shape[0])]
+    # co = test_labels.tocoo()
+    # for i, j in zip(co.row, co.col):
+    #     y_true[i].append(j)
+
+    score_types = ['micro', 'macro']
+    train_ratios = [0.1, 0.5, 0.9]
+    results = {score_type: {train_ratio: 0. for train_ratio in train_ratios} for score_type in score_types}
+    for train_ratio in train_ratios:
+
+        mlb = MultiLabelBinarizer(range(K))
+        for score_t in score_types:
+            score = f1_score(y_true=mlb.fit_transform(y_true),
+                             y_pred=mlb.fit_transform(y_pred),
+                             average=score_t)
+
+            results[score_t][train_ratio] = score
+
+    for score_t in score_types:
+        print(score_t, [results[score_t][train_ratio] for train_ratio in train_ratios])
+
+    # Simon's baseline
+    print("Simon's baseline")
+    max_row_idx = max(mat_indices.T[0])+1
+    label_counts = np.zeros(shape=(max_row_idx, K+1), dtype=np.int)
+    for row_idx, col_idx in mat_indices:
+        label_counts[row_idx, data['events'][row_idx, col_idx]] += 1
+
+    # estimated labels
+    y_pred = [[label_counts[row_idx, 1:].argmax()] for row_idx, col_idx in mat_indices]
+
+    results = {score_type: {train_ratio: 0. for train_ratio in train_ratios} for score_type in score_types}
+    for train_ratio in train_ratios:
+
+        mlb = MultiLabelBinarizer(range(K))
+        for score_t in score_types:
+            score = f1_score(y_true=mlb.fit_transform(y_true),
+                             y_pred=mlb.fit_transform(y_pred),
+                             average=score_t)
+
+            results[score_t][train_ratio] = score
+
+    for score_t in score_types:
+        print(score_t, [results[score_t][train_ratio] for train_ratio in train_ratios])
+
+
+    print(p2v.get_variable_at(i=0, order_index=0, t=0.5) )
+    print(p2v.get_variable_at(i=0, order_index=1, t=0.5))
+    print(p2v.get_variable_at(i=0, order_index=2, t=0.5) )
+    print(p2v.get_variable_at(i=0, order_index=2, t=0.5) )
+    # print(p2v.get_all_row_positions(mat_indices=, col_times=[0.5]) )

models/pol2vecbinary.py
@@ -6,7 +6,7 @@ from torch import sigmoid
 from torch.nn.functional import logsigmoid as log_sigmoid
 
 
-class Pol2Vec(torch.nn.Module):
+class Pol2VecBinary(torch.nn.Module):
 
     def __init__(self, train_data, dim, order, epochs_num, learning_rate, samples_num=10, seed=123):
         super().__init__()
@@ -16,15 +16,16 @@ class Pol2Vec(torch.nn.Module):
         torch.manual_seed(self.__seed)
         np.random.seed(self.__seed)
 
-        self.__train_events = torch.tensor(train_data['events'], dtype=torch.int8)
+        self.__train_events = train_data['events']
+        self.__train_col_indices = train_data['col_indices']
         self.__train_event_times = train_data['times']
 
         # print(self.__train_event_times)
 
         # self.__test_data = test_data
 
-        self.__row_size = self.__train_events.shape[0]
-        self.__col_size = self.__train_events.shape[1]
+        self.__row_size = self.__train_events[0].shape[0] #None #self.__train_events.shape[0]
+        self.__col_size = sum([len(event_times) for event_times in self.__train_event_times])  #None #self.__train_events.shape[1]
 
         self.__dim = dim
         self.__var_size = order + 1
@@ -43,8 +44,6 @@ class Pol2Vec(torch.nn.Module):
         # Store the factorial terms
         self.__factorials = [float(math.factorial(o)) for o in range(self.__var_size)]
 
-        self.__pdist = torch.nn.PairwiseDistance(p=2, keepdim=False)
-
     def get_variable_at(self, i, order_index, t):
 
         z = torch.zeros(size=(self.__dim,), dtype=torch.float)
@@ -57,14 +56,14 @@ class Pol2Vec(torch.nn.Module):
 
         return self.get_variable_at(i=i, order_index=0, t=t)
 
-    def get_all_row_positions(self, col_times):
+    def get_all_row_positions(self, col_times, col_indices):
 
-        z = torch.zeros(size=(self.__row_size, self.__dim, self.__col_size ), dtype=torch.float)
+        z = torch.zeros(size=(self.__row_size, self.__dim, len(col_times) ), dtype=torch.float)
         col_times_mat = torch.tensor([(np.asarray(col_times) ** o) / math.factorial(o) for o in range(self.__var_size)], dtype=torch.float )
 
-        for col_idx, col_t in enumerate(col_times):
+        for idx, col_t in enumerate(col_times):
             for o in range(self.__var_size):
-                z[:, :, col_idx] += self.__z_rows[o] * col_times_mat[o, col_idx]
+                z[:, :, idx] += self.__z_rows[o] * col_times_mat[o, idx]
 
         return z
 
@@ -80,12 +79,13 @@ class Pol2Vec(torch.nn.Module):
         temp = z_row - z_col
         return torch.sqrt(torch.dot(temp, temp))
 
-    def get_all_pairwise_distances(self, col_times):
+    def get_all_pairwise_distances(self, col_indices, col_times):
 
-        z_rows = self.get_all_row_positions(col_times=col_times)  # row_size x dim x col_size
-
+        z_rows = self.get_all_row_positions(col_times=col_times, col_indices=col_indices)  # row_size x dim x col_size
+        # print(z_rows.permute(2, 0, 1).shape)
+        # print(self.__z_cols[col_indices, :].unsqueeze(1).shape)
         # cdsit output matrix = col_size x row_size x 1
-        return torch.cdist(z_rows.permute(2, 0, 1), self.__z_cols.unsqueeze(1), p=2).squeeze().transpose(1, 0)
+        return torch.cdist(z_rows.permute(2, 0, 1), self.__z_cols[col_indices, :].unsqueeze(1), p=2).squeeze().transpose(1, 0)
 
     def get_col_bias(self, col_idx):
 
@@ -95,29 +95,35 @@ class Pol2Vec(torch.nn.Module):
 
         return self.__gamma_rows[row_idx]
 
-    def get_all_col_bias(self):
+    def get_all_col_bias(self, indices=None):
 
-        return self.__gamma_cols
+        if indices is None:
+            return self.__gamma_cols
+        else:
+            return self.__gamma_cols[indices]
 
-    def get_all_row_bias(self):
+    def get_all_row_bias(self, indices=None):
 
-        return self.__gamma_rows
+        if indices is None:
+            return self.__gamma_rows
+        else:
+            return self.__gamma_rows[indices]
 
     def compute_likelihood_for(self, row_idx, col_idx, t):
 
         return sigmoid( self.get_row_bias(row_idx) + self.get_col_bias(col_idx) - self.get_distance(row_idx, col_idx, t) )
 
-    def compute_all_likelihood(self, col_times):
+    def compute_all_likelihood(self, col_indices, col_times):
 
-        dist = -self.get_all_pairwise_distances(col_times=col_times)
+        dist = -self.get_all_pairwise_distances(col_indices=col_indices, col_times=col_times)
 
         dist += self.get_all_row_bias()[:, None]
-        dist += self.get_all_col_bias()[None, :]
+        dist += self.get_all_col_bias(indices=col_indices)[None, :]
 
         return sigmoid(dist)
 
     def compute_log_likelihood(self, col_idx, col_events, col_time):
-        self.compute_all_likelihood(col_events)
+
         log_likelihood = 0.
         for node in range(self.__row_size):
 
@@ -129,7 +135,7 @@ class Pol2Vec(torch.nn.Module):
 
         return log_likelihood
 
-    def compute_all_log_likelihood(self, event_mat, col_times):
+    def compute_all_log_likelihood(self, event_mat, col_indices, col_times):
         '''
 
         :param event_mat: row_size x col_size matrix
@@ -137,23 +143,16 @@ class Pol2Vec(torch.nn.Module):
         :return:
         '''
 
-        p_mat = (1-event_mat) - (1 - 2*event_mat) * self.compute_all_likelihood(col_times=col_times)
+        temp = self.compute_all_likelihood(col_indices=col_indices, col_times=col_times)
+        # print(temp.shape)
+        # print(event_mat.shape)
+        p_mat = (1-event_mat) - (1 - 2*event_mat) * temp
 
         return torch.sum(torch.log(p_mat))
 
-    def forward(self, events, events_time):
+    def forward(self, events, col_indices, events_time):
 
-        neg_logLikelihood = 0.
-        # for col_idx in range(self.__col_size):
-        #
-        #     s = self.compute_log_likelihood(col_idx=col_idx, col_events=events[:, col_idx],
-        #                                                      col_time=events_time[col_idx])
-        #     # print(bill_idx, s)
-        #     neg_logLikelihood += s
-
-        neg_logLikelihood = self.compute_all_log_likelihood(event_mat=self.__train_events, col_times=self.__train_event_times)
-
-        return -neg_logLikelihood
+        return -self.compute_all_log_likelihood(event_mat=events, col_indices=col_indices, col_times=events_time)
 
     def set_gradients_of_latent_variables(self, index, value=True):
 
@@ -185,22 +184,27 @@ class Pol2Vec(torch.nn.Module):
                 optimizer = torch.optim.Adam(self.parameters(), lr=self.__lr)
 
                 for epoch in range(self.__epochs_num):
-
+                    # print("Epoch: {}".format(epoch))
                     self.train()
 
                     for param in self.parameters():
                         param.grad = None
 
                     # Forward pass
-                    train_loss = self.forward(events=self.__train_events, events_time=self.__train_event_times)
+                    print("forward")
+                    total_train_loss = 0.
+                    for batch_train_events, col_indices, batch_event_times in zip(self.__train_events, self.__train_col_indices, self.__train_event_times):
+                        train_loss = self.forward(events=torch.tensor(batch_train_events.todense(), dtype=torch.int8),
+                                                  col_indices=col_indices, events_time=batch_event_times)
+                        total_train_loss += train_loss.item()
+                        # Backward pass
+                        train_loss.backward()
+                        # Step
+                        optimizer.step()
 
-                    # print(train_loss)
-                    # Backward pass
-                    train_loss.backward()
-                    # Step
-                    optimizer.step()
+                    print("post-forward")
                     # Append the loss
-                    train_loss_list.append(train_loss.item() / self.__col_size)
+                    train_loss_list.append(total_train_loss / self.__col_size)
 
                     # self.eval()
                     # with torch.no_grad():
@@ -208,7 +212,7 @@ class Pol2Vec(torch.nn.Module):
                     #     testLoss.append(test_loss / len(self.__testSet))
 
                     if epoch % 50 == 0:
-                        print(f"Epoch {epoch + 1} train loss: {train_loss.item() / self.__col_size}")
+                        print(f"Epoch {epoch + 1} train loss: {total_train_loss.item() / self.__col_size}")
                         # print(f"Epoch {epoch + 1} test loss: {test_loss / len(self.__testSet)}")
 
         return train_loss_list, test_loss_list

models/pol2vecmulti.py
@@ -4,11 +4,14 @@ import time
 import numpy as np
 from torch import sigmoid
 from torch.nn.functional import logsigmoid as log_sigmoid
+from torch.distributions.normal import Normal
 
 
-class Pol2VecBinary(torch.nn.Module):
+class Pol2VecMulti(torch.nn.Module):
 
-    def __init__(self, train_data, dim, order, epochs_num, learning_rate, samples_num=10, seed=123):
+    def __init__(self, train_data, dim, order, metric, metric_param=None, epochs_num=100,
+                 learning_rate=0.1, sigma_grad=False, seed=123, verbose=True):
+
         super().__init__()
 
         # Set the seed value
@@ -23,26 +26,63 @@ class Pol2VecBinary(torch.nn.Module):
         # print(self.__train_event_times)
 
         # self.__test_data = test_data
-
         self.__row_size = self.__train_events[0].shape[0] #None #self.__train_events.shape[0]
         self.__col_size = sum([len(event_times) for event_times in self.__train_event_times])  #None #self.__train_events.shape[1]
 
         self.__dim = dim
         self.__var_size = order + 1
 
+        self.__metric = metric
+        self.__metric_param = metric_param
+
         # Optimization parameters
         self.__epochs_num = epochs_num
         self.__lr = learning_rate
 
         # Set the models parameters
-        self.__gamma_cols = torch.nn.Parameter(torch.rand(size=(self.__col_size,)), requires_grad=False)
-        self.__gamma_rows = torch.nn.Parameter(torch.rand(size=(self.__row_size,)), requires_grad=False)
-        self.__z_cols = torch.nn.Parameter(torch.rand(size=(self.__col_size, self.__dim), ), requires_grad=True)
-        self.__z_rows = [torch.rand(size=(self.__row_size, self.__dim)) for _ in range(self.__var_size)]
-        self.__z_rows = torch.nn.ParameterList([torch.nn.Parameter(var, requires_grad=False) for var in self.__z_rows])
+        self.__gamma_cols = torch.nn.Parameter(2*torch.rand(size=(self.__col_size,))-1., requires_grad=False)
+        self.__gamma_rows = torch.nn.Parameter(2*torch.rand(size=(self.__row_size,))-1., requires_grad=False)
+        self.__z_cols = torch.nn.Parameter(2*torch.rand(size=(self.__col_size, self.__dim))-1., requires_grad=True)
+        # self.__z_rows = [torch.rand(size=(self.__row_size, self.__dim)) for _ in range(self.__var_size)]
+        # self.__z_rows = torch.nn.ParameterList([torch.nn.Parameter(var, requires_grad=False) for var in self.__z_rows])
+        self.__z_rows = torch.nn.Parameter(2*torch.rand(size=(self.__var_size, self.__row_size, self.__dim))-1., requires_grad=True)
+
+        # Class num is the number of different labels except 0
+        self.__class_num = len(set(e for event_mat in self.__train_events for e in event_mat.data.tolist()))
+
+        self.__b = torch.nn.Parameter(torch.tensor(np.linspace(-1., 1., self.__class_num-1).tolist(), dtype=torch.float))
+        self.__sigma = torch.nn.Parameter(torch.tensor([1.]), requires_grad=sigma_grad)
+
+        # A matrix for Mahalanobis distance
+        self.__L = 2 * torch.zeros(size=(self.__col_size, self.__dim, self.__dim)) - 1.
+        for col_idx in range(self.__col_size):
+            if self.__metric_param == "diag" or metric_param == "diagonal":
+                self.__L[col_idx, :, :] = torch.diag(2 * torch.rand(size=(self.__dim, )) - 1.)
+            elif self.__metric_param == "full":
+                self.__L[col_idx, :, :] = torch.tril(2 * torch.rand(size=(self.__dim, self.__dim)) - 1.)
+            else:
+                raise ValueError("Invalid metric parameter value!")
+        self.__L = torch.nn.Parameter(self.__L, requires_grad=True)
+
+        print(self.__L.shape)
 
         # Store the factorial terms
         self.__factorials = [float(math.factorial(o)) for o in range(self.__var_size)]
+
+        # Define the normal distribution
+        self.__normal = Normal(0, 1)
+
+        # Define a big number
+        self.__big_number = 1e+5
+
+        #
+        self.__b.data[(self.__class_num-1)//2] = 0.
+
+        if verbose:
+            print("Number of classes: {}".format(self.__class_num))
+
+        # Distance function
+        self.__pdist = torch.nn.PairwiseDistance(p=2)
 
     def get_variable_at(self, i, order_index, t):
 
@@ -56,15 +96,21 @@ class Pol2VecBinary(torch.nn.Module):
 
         return self.get_variable_at(i=i, order_index=0, t=t)
 
-    def get_all_row_positions(self, col_times, col_indices):
+    def get_all_row_positions(self, mat_indices, col_times):
 
-        z = torch.zeros(size=(self.__row_size, self.__dim, len(col_times) ), dtype=torch.float)
-        col_times_mat = torch.tensor([(np.asarray(col_times) ** o) / math.factorial(o) for o in range(self.__var_size)], dtype=torch.float )
+        # z = torch.zeros(size=(len(mat_indices), self.__dim), dtype=torch.float)
+        t0 = time.time()
+        col_times_mat = torch.tensor([(np.asarray(col_times) ** o) / self.__factorials[o] for o in range(self.__var_size)], dtype=torch.float )
+        # print("Col times: {}".format(time.time() - t0))
 
-        for idx, col_t in enumerate(col_times):
-            for o in range(self.__var_size):
-                z[:, :, idx] += self.__z_rows[o] * col_times_mat[o, idx]
+        t0 = time.time()
+        # for idx, mat_idx in enumerate(mat_indices):
+        #     z[idx, :] = torch.matmul(col_times_mat[:, mat_idx[1]], self.__z_rows[:, mat_idx[0], :])
 
+        z = torch.matmul(self.__z_rows.transpose(0, 2), col_times_mat).permute(1, 2, 0) #.reshape(-1, self.__dim)
+        z = z[mat_indices.T[0], mat_indices.T[1], :]
+        # print("z assignment: {}".format(time.time() - t0))
+        # print(z.shape)
         return z
 
     def get_position_of_column(self, col_idx):
@@ -79,13 +125,28 @@ class Pol2VecBinary(torch.nn.Module):
         temp = z_row - z_col
         return torch.sqrt(torch.dot(temp, temp))
 
-    def get_all_pairwise_distances(self, col_indices, col_times):
+    def get_all_pairwise_distances(self, mat_indices, col_idx_list, col_times):
 
-        z_rows = self.get_all_row_positions(col_times=col_times, col_indices=col_indices)  # row_size x dim x col_size
-        # print(z_rows.permute(2, 0, 1).shape)
-        # print(self.__z_cols[col_indices, :].unsqueeze(1).shape)
-        # cdsit output matrix = col_size x row_size x 1
-        return torch.cdist(z_rows.permute(2, 0, 1), self.__z_cols[col_indices, :].unsqueeze(1), p=2).squeeze().transpose(1, 0)
+        t0 = time.time()
+        z_rows = self.get_all_row_positions(mat_indices=mat_indices, col_times=col_times)
+
+        if self.__metric == "euc" or self.__metric == "euclidean":
+
+            return self.__pdist(z_rows, self.__z_cols[mat_indices.T[1], :])
+
+        elif self.__metric == "mah" or self.__metric == "mahalanobis":
+
+            chosen_L = self.__L[col_idx_list, :, :]
+            # precision_matrix = torch.matmul(chosen_L, chosen_L.transpose(0, 1))
+
+            delta = z_rows - self.__z_cols[mat_indices.T[1], :]
+            T = torch.matmul(delta.unsqueeze(1), chosen_L[mat_indices.T[1], :, :])
+
+            return torch.norm(T.squeeze(1), p=2, dim=1)
+
+        else:
+
+            raise ValueError("Invalid metric name!")
 
     def get_col_bias(self, col_idx):
 
@@ -113,29 +174,16 @@ class Pol2VecBinary(torch.nn.Module):
 
         return sigmoid( self.get_row_bias(row_idx) + self.get_col_bias(col_idx) - self.get_distance(row_idx, col_idx, t) )
 
-    def compute_all_likelihood(self, col_indices, col_times):
+    def compute_all_f(self, mat_indices, col_idx_list, col_times):
 
-        dist = -self.get_all_pairwise_distances(col_indices=col_indices, col_times=col_times)
+        dist = -self.get_all_pairwise_distances(mat_indices=mat_indices, col_idx_list=col_idx_list, col_times=col_times)
 
-        dist += self.get_all_row_bias()[:, None]
-        dist += self.get_all_col_bias(indices=col_indices)[None, :]
+        dist += self.get_all_row_bias(indices=mat_indices.T[0])
+        dist += self.get_all_col_bias(indices=mat_indices.T[1])
 
-        return sigmoid(dist)
+        return dist
 
-    def compute_log_likelihood(self, col_idx, col_events, col_time):
-
-        log_likelihood = 0.
-        for node in range(self.__row_size):
-
-            p = self.compute_likelihood_for(row_idx=node, col_idx=col_idx, t=col_time)
-            if col_events[node]:
-                log_likelihood += torch.log( p )
-            else:
-                log_likelihood += torch.log( 1. - p)
-
-        return log_likelihood
-
-    def compute_all_log_likelihood(self, event_mat, col_indices, col_times):
+    def compute_all_log_likelihood(self, event_mat, col_idx_list, col_times):
         '''
 
         :param event_mat: row_size x col_size matrix
@@ -143,20 +191,62 @@ class Pol2VecBinary(torch.nn.Module):
         :return:
         '''
 
-        temp = self.compute_all_likelihood(col_indices=col_indices, col_times=col_times)
-        # print(temp.shape)
-        # print(event_mat.shape)
-        p_mat = (1-event_mat) - (1 - 2*event_mat) * temp
+        t0 = time.time()
+        mat_indices = event_mat.nonzero()
+        # print("Step0: ", time.time() - t0)
 
-        return torch.sum(torch.log(p_mat))
+        t0 = time.time()
+        temp = self.compute_all_f(mat_indices=mat_indices, col_idx_list=col_idx_list, col_times=col_times)
+        # print("Step1: ", time.time() - t0)
+        # print(temp)
+        t0 = time.time()
+        theta = torch.hstack((torch.tensor([-self.__big_number]), self.__b, torch.tensor([self.__big_number])))
+        # print("Step2: ", time.time() - t0)
 
-    def forward(self, events, col_indices, events_time):
+        t0 = time.time()
+        y0, y1 = [], []
+        for row_idx, col_idx in mat_indices:
+            y0.append(event_mat[row_idx, col_idx] - 1)
+            y1.append(event_mat[row_idx, col_idx])
+        # y0 = [event_mat[row_idx, col_idx] - 1 for row_idx, col_idx in mat_indices]
+        # y1 = [event_mat[row_idx, col_idx] for row_idx, col_idx in mat_indices]
+        # print("Step3: ", time.time() - t0)
 
-        return -self.compute_all_log_likelihood(event_mat=events, col_indices=col_indices, col_times=events_time)
+        t0 = time.time()
+        ll = torch.log(self.__normal.cdf((theta[y1] - temp)/self.__sigma) - self.__normal.cdf((theta[y0] - temp)/self.__sigma))
+        # print("Step4: ", time.time() - t0)
+
+        return ll.sum()
+
+    def forward(self, events, col_idx_list, events_time):
+
+        return -self.compute_all_log_likelihood(event_mat=events, col_idx_list=col_idx_list, col_times=events_time)
 
     def set_gradients_of_latent_variables(self, index, value=True):
 
-        self.__z_rows[index].requires_grad = value
+        # self.__z_rows[index].requires_grad = value
+        pass
+
+    def __set_gradients(self, inx):
+
+        #
+        self.__b.grad[(self.__class_num-1)//2] = 0
+
+        #
+        for col_idx in range(self.__col_size):
+            if self.__metric_param == "full":
+
+                self.__L.grad[col_idx, torch.triu_indices(row=self.__dim, col=self.__dim, offset=1)] = 0
+
+            if self.__metric_param == "diag" or self.__metric_param == "diagonal":
+
+                self.__L.grad[col_idx, range(self.__dim),  range(self.__dim)] = 0
+
+
+
+        #
+        if inx > 0:
+            self.__z_rows.grad[inx:, :, :] = 0
 
     def set_gradient_of_bias_terms(self, value=True):
 
@@ -191,20 +281,25 @@ class Pol2VecBinary(torch.nn.Module):
                         param.grad = None
 
                     # Forward pass
-                    print("forward")
+                    # print("forward")
                     total_train_loss = 0.
+                    counter = 0
                     for batch_train_events, col_indices, batch_event_times in zip(self.__train_events, self.__train_col_indices, self.__train_event_times):
-                        train_loss = self.forward(events=torch.tensor(batch_train_events.todense(), dtype=torch.int8),
-                                                  col_indices=col_indices, events_time=batch_event_times)
-                        total_train_loss += train_loss.item()
-                        # Backward pass
-                        train_loss.backward()
-                        # Step
-                        optimizer.step()
+                        # print(counter)
+                        counter += 1
+                        train_loss = self.forward(events=torch.tensor(batch_train_events.todense(), dtype=torch.int8), col_idx_list=col_indices, events_time=batch_event_times)
+                        total_train_loss += train_loss
+                    # Backward pass
+                    total_train_loss.backward()
 
-                    print("post-forward")
+                    self.__set_gradients(inx=inx)
+
+                    # Step
+                    optimizer.step()
+                    # print(self.__b)
+                    # print("post-forward")
                     # Append the loss
-                    train_loss_list.append(total_train_loss / self.__col_size)
+                    train_loss_list.append(total_train_loss.item() / self.__col_size)
 
                     # self.eval()
                     # with torch.no_grad():
@@ -217,3 +312,19 @@ class Pol2VecBinary(torch.nn.Module):
 
         return train_loss_list, test_loss_list
 
+    def get_label_assignments(self, mat_indices, col_times):
+
+        num_of_elements = len(mat_indices)
+        values = self.compute_all_f(mat_indices, col_times)
+
+        assigned_y = [0 for _ in range(num_of_elements)]
+        for i in range(num_of_elements):
+            c = 0
+            for b in self.__b:
+                if values[i] <= b:
+                    break
+                c += 1
+
+            assigned_y[i] = c
+
+        return assigned_y